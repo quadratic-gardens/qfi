b745f5e3bcb822755ec3615a41c3a906e8c74797
Jeeiii <giacomo.corrias7@gmail.com>
2022-05-31 12:54:17 +0200

    refactor: add missing try-catch blocks

--- packages/cli/src/commands/deploy.ts
+++ packages/cli/src/commands/deploy.ts
@@ -25,108 +25,109 @@ import { directoryExists, makeDir, writeLocalJsonFile } from "../lib/files.js"
  * @param network <string> - the network where the contracts are going to be deployed.
  */
 async function deploy(network: string) {
-    const outputDirPath = `./output`
+    try {
+        const outputDirPath = `./output`
 
-    // Get the provider.
-    const provider = getProvider(network)
-    console.log(`${logSymbols.success} Connected to ${provider.network.name} / Chain ID ${provider.network.chainId}`)
+        // Get the provider.
+        const provider = getProvider(network)
+        console.log(`${logSymbols.success} Connected to ${provider.network.name} / Chain ID ${provider.network.chainId}`)
 
-    // Connect wallet to provider from mnemonic.
-    const wallet = connectWalletToProviderFromMnemonic(provider)
-    const balanceInEthers = ethers.utils.formatEther((await wallet.getBalance()).toString())
+        // Connect wallet to provider from mnemonic.
+        const wallet = connectWalletToProviderFromMnemonic(provider)
+        const balanceInEthers = ethers.utils.formatEther((await wallet.getBalance()).toString())
 
-    console.log(`${logSymbols.success} Deployer address -> ${wallet.address}`)
-    console.log(`${logSymbols.success} Current balance (Ξ): ${balanceInEthers}`)
+        console.log(`${logSymbols.success} Deployer address -> ${wallet.address}`)
+        console.log(`${logSymbols.success} Current balance (Ξ): ${balanceInEthers}`)
 
-    console.log(`\n${logSymbols.info} Starting the deploy for QFI/MACI smart contracts`)
+        console.log(`\n${logSymbols.info} Starting the deploy for QFI/MACI smart contracts`)
 
-    /** DEPLOY MACI/QFI SMART CONTRACTS */
-    
-    const PoseidonT3Factory = new PoseidonT3__factory(wallet);
-    const PoseidonT4Factory = new PoseidonT4__factory(wallet);
-    const PoseidonT5Factory = new PoseidonT5__factory(wallet);
-    const PoseidonT6Factory = new PoseidonT6__factory(wallet);
-  
-    const PoseidonT3 = await PoseidonT3Factory.deploy();
-    console.log(`${logSymbols.success} PoseidonT3 deployed at ${PoseidonT3.address}`);
+        /** DEPLOY MACI/QFI SMART CONTRACTS */
 
-    const PoseidonT4 = await PoseidonT4Factory.deploy();
-    console.log(`${logSymbols.success} PoseidonT4 deployed at ${PoseidonT4.address}`);
+        const PoseidonT3Factory = new PoseidonT3__factory(wallet);
+        const PoseidonT4Factory = new PoseidonT4__factory(wallet);
+        const PoseidonT5Factory = new PoseidonT5__factory(wallet);
+        const PoseidonT6Factory = new PoseidonT6__factory(wallet);
 
-    const PoseidonT5 = await PoseidonT5Factory.deploy();
-    console.log(`${logSymbols.success} PoseidonT5 deployed at ${PoseidonT5.address}`);
+        const PoseidonT3 = await PoseidonT3Factory.deploy();
+        console.log(`${logSymbols.success} PoseidonT3 deployed at ${PoseidonT3.address}`);
 
-    const PoseidonT6 = await PoseidonT6Factory.deploy();
-    console.log(`${logSymbols.success} PoseidonT6 deployed at ${PoseidonT6.address}`);
+        const PoseidonT4 = await PoseidonT4Factory.deploy();
+        console.log(`${logSymbols.success} PoseidonT4 deployed at ${PoseidonT4.address}`);
 
-    const linkedLibraryAddresses = {
-        ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT5"]: PoseidonT5.address,
-        ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT3"]: PoseidonT3.address,
-        ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT6"]: PoseidonT6.address,
-        ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT4"]: PoseidonT4.address,
-    };
+        const PoseidonT5 = await PoseidonT5Factory.deploy();
+        console.log(`${logSymbols.success} PoseidonT5 deployed at ${PoseidonT5.address}`);
 
-    const GrantRoundFactory = new GrantRoundFactory__factory({ ...linkedLibraryAddresses }, wallet);
-    const PollFactoryFactory = new PollFactory__factory({ ...linkedLibraryAddresses }, wallet);
-    const MessageAqFactoryFactory = new MessageAqFactory__factory({ ...linkedLibraryAddresses }, wallet);
-    const QFIFactory = new QFI__factory({ ...linkedLibraryAddresses }, wallet);
-  
-    const VKRegistryFactory = new VkRegistry__factory(wallet);
-    const ConstantInitialVoiceCreditProxyFactory = new ConstantInitialVoiceCreditProxy__factory(wallet);
-    const FreeForAllGateKeeperFactory = new FreeForAllGatekeeper__factory(wallet);
-    const RecipientRegistryFactory = new OptimisticRecipientRegistry__factory(wallet);
-    const BaseERC20TokenFactory = new BaseERC20Token__factory(wallet);
-    const PollProcessorAndTallyerFactory = new PollProcessorAndTallyer__factory(wallet);
-    const MockVerifierFactory = new MockVerifier__factory(wallet);
-  
-    const optimisticRecipientRegistry = await RecipientRegistryFactory.deploy(0, 0, wallet.address);
-    console.log(`${logSymbols.success} OptimisticRecipientRegistry deployed at ${optimisticRecipientRegistry.address}`);
+        const PoseidonT6 = await PoseidonT6Factory.deploy();
+        console.log(`${logSymbols.success} PoseidonT6 deployed at ${PoseidonT6.address}`);
 
-    const grantRoundFactory = await GrantRoundFactory.deploy();
-    console.log(`${logSymbols.success} GrantRoundFactory deployed at ${grantRoundFactory.address}`);
+        const linkedLibraryAddresses = {
+            ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT5"]: PoseidonT5.address,
+            ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT3"]: PoseidonT3.address,
+            ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT6"]: PoseidonT6.address,
+            ["qaci-contracts/contracts/crypto/Hasher.sol:PoseidonT4"]: PoseidonT4.address,
+        };
 
-    const tx = await grantRoundFactory.setRecipientRegistry(optimisticRecipientRegistry.address);
-    await tx.wait()
-    console.log(`${logSymbols.success} GrantRoundFactory registry correctly set!`);
+        const GrantRoundFactory = new GrantRoundFactory__factory({ ...linkedLibraryAddresses }, wallet);
+        const PollFactoryFactory = new PollFactory__factory({ ...linkedLibraryAddresses }, wallet);
+        const MessageAqFactoryFactory = new MessageAqFactory__factory({ ...linkedLibraryAddresses }, wallet);
+        const QFIFactory = new QFI__factory({ ...linkedLibraryAddresses }, wallet);
 
-    const pollFactory = await PollFactoryFactory.deploy();
-    console.log(`${logSymbols.success} PollFactory deployed at ${pollFactory.address}`);
+        const VKRegistryFactory = new VkRegistry__factory(wallet);
+        const ConstantInitialVoiceCreditProxyFactory = new ConstantInitialVoiceCreditProxy__factory(wallet);
+        const FreeForAllGateKeeperFactory = new FreeForAllGatekeeper__factory(wallet);
+        const RecipientRegistryFactory = new OptimisticRecipientRegistry__factory(wallet);
+        const BaseERC20TokenFactory = new BaseERC20Token__factory(wallet);
+        const PollProcessorAndTallyerFactory = new PollProcessorAndTallyer__factory(wallet);
+        const MockVerifierFactory = new MockVerifier__factory(wallet);
 
-    const messageAqFactory = await MessageAqFactoryFactory.deploy();
-    console.log(`${logSymbols.success} MessageAqFactory deployed at ${messageAqFactory.address}`);
+        const optimisticRecipientRegistry = await RecipientRegistryFactory.deploy(0, 0, wallet.address);
+        console.log(`${logSymbols.success} OptimisticRecipientRegistry deployed at ${optimisticRecipientRegistry.address}`);
 
-    const messageAqFactoryGrants = await MessageAqFactoryFactory.deploy();
-    console.log(`${logSymbols.success} MessageAqFactoryGrants deployed at ${messageAqFactoryGrants.address}`);
+        const grantRoundFactory = await GrantRoundFactory.deploy();
+        console.log(`${logSymbols.success} GrantRoundFactory deployed at ${grantRoundFactory.address}`);
 
-    const freeForAllGateKeeper = await FreeForAllGateKeeperFactory.deploy();
-    console.log(`${logSymbols.success} FreeForAllGateKeeper deployed at ${freeForAllGateKeeper.address}`);
+        const tx = await grantRoundFactory.setRecipientRegistry(optimisticRecipientRegistry.address);
+        await tx.wait()
+        console.log(`${logSymbols.success} GrantRoundFactory registry correctly set!`);
 
-    const constantInitialVoiceCreditProxy = await ConstantInitialVoiceCreditProxyFactory.deploy(0);
-    console.log(`${logSymbols.success} ConstantInitialVoiceCreditProxy deployed at ${constantInitialVoiceCreditProxy.address}`);
+        const pollFactory = await PollFactoryFactory.deploy();
+        console.log(`${logSymbols.success} PollFactory deployed at ${pollFactory.address}`);
 
-    const vkRegistry = await VKRegistryFactory.deploy();
-    console.log(`${logSymbols.success} VKRegistry deployed at ${vkRegistry.address}`);
+        const messageAqFactory = await MessageAqFactoryFactory.deploy();
+        console.log(`${logSymbols.success} MessageAqFactory deployed at ${messageAqFactory.address}`);
 
-    const baseERC20Token = await BaseERC20TokenFactory.deploy(100);
-    console.log(`${logSymbols.success} BaseERC20Token deployed at ${baseERC20Token.address}`);
+        const messageAqFactoryGrants = await MessageAqFactoryFactory.deploy();
+        console.log(`${logSymbols.success} MessageAqFactoryGrants deployed at ${messageAqFactoryGrants.address}`);
 
-    const mockVerifier = await MockVerifierFactory.deploy();
-    console.log(`${logSymbols.success} MockVerifier deployed at ${mockVerifier.address}`);
+        const freeForAllGateKeeper = await FreeForAllGateKeeperFactory.deploy();
+        console.log(`${logSymbols.success} FreeForAllGateKeeper deployed at ${freeForAllGateKeeper.address}`);
 
-    const pollProcessorAndTallyer = await PollProcessorAndTallyerFactory.deploy(mockVerifier.address);
-    console.log(`${logSymbols.success} PollProcessorAndTallyer deployed at ${pollProcessorAndTallyer.address}`);
+        const constantInitialVoiceCreditProxy = await ConstantInitialVoiceCreditProxyFactory.deploy(0);
+        console.log(`${logSymbols.success} ConstantInitialVoiceCreditProxy deployed at ${constantInitialVoiceCreditProxy.address}`);
 
-    const qfi = await QFIFactory.deploy(
-        baseERC20Token.address,
-        grantRoundFactory.address,
-        pollFactory.address,
-        freeForAllGateKeeper.address,
-        constantInitialVoiceCreditProxy.address
-    );
-    console.log(`${logSymbols.success} QFI deployed at ${qfi.address}`);
+        const vkRegistry = await VKRegistryFactory.deploy();
+        console.log(`${logSymbols.success} VKRegistry deployed at ${vkRegistry.address}`);
 
-    // Store addresses in a local JSON file.
-    const contractsInJson = `{
+        const baseERC20Token = await BaseERC20TokenFactory.deploy(100);
+        console.log(`${logSymbols.success} BaseERC20Token deployed at ${baseERC20Token.address}`);
+
+        const mockVerifier = await MockVerifierFactory.deploy();
+        console.log(`${logSymbols.success} MockVerifier deployed at ${mockVerifier.address}`);
+
+        const pollProcessorAndTallyer = await PollProcessorAndTallyerFactory.deploy(mockVerifier.address);
+        console.log(`${logSymbols.success} PollProcessorAndTallyer deployed at ${pollProcessorAndTallyer.address}`);
+
+        const qfi = await QFIFactory.deploy(
+            baseERC20Token.address,
+            grantRoundFactory.address,
+            pollFactory.address,
+            freeForAllGateKeeper.address,
+            constantInitialVoiceCreditProxy.address
+        );
+        console.log(`${logSymbols.success} QFI deployed at ${qfi.address}`);
+
+        // Store addresses in a local JSON file.
+        const contractsInJson = `{
         "PoseidonT3": \"${PoseidonT3.address}\",
         "PoseidonT4": \"${PoseidonT4.address}\",
         "PoseidonT5": \"${PoseidonT5.address}\",
@@ -145,14 +146,17 @@ async function deploy(network: string) {
         "QFI": \"${qfi.address}\"
     }`
 
-    if (!directoryExists(outputDirPath))
-        makeDir(outputDirPath)
+        if (!directoryExists(outputDirPath))
+            makeDir(outputDirPath)
 
-    writeLocalJsonFile(`./output/deployedContracts.json`, JSON.parse(contractsInJson))
+        writeLocalJsonFile(`./output/deployedContracts.json`, JSON.parse(contractsInJson))
 
-    console.log(
-        `\nYou have successfully deployed the MACI/QFI smart contracts 🎊 You can find everything inside the \`deployedContracts.json\` file!`
-      )
+        console.log(
+            `\nYou have successfully deployed the MACI/QFI smart contracts 🎊 You can find everything inside the \`deployedContracts.json\` file!`
+        )
+    } catch (err: any) {
+        console.log(`${logSymbols.error} Something went wrong: ${err}`)
+    }
 }
 
 export default deploy
--- packages/cli/src/commands/genkeys.ts
+++ packages/cli/src/commands/genkeys.ts
@@ -10,77 +10,81 @@ import toQRCodeFile from "../lib/qrcode.js"
  * @param quantity <number> - amount of MACI and ETH keypairs to be generated.
  */
 async function genkeys(quantity: number) {
-  const dirPath = `./output`
-  const ethDirPathName = `ethKeys`
-  const maciDirPathName = `maciKeys`
+  try {
+    const dirPath = `./output`
+    const ethDirPathName = `ethKeys`
+    const maciDirPathName = `maciKeys`
 
-  const ethBaseName = `eth_key`
-  const maciBaseName = `maci_key`
+    const ethBaseName = `eth_key`
+    const maciBaseName = `maci_key`
 
-  const ethKeysDirPath = `${dirPath}/${ethDirPathName}`
-  const maciKeysDirPath = `${dirPath}/${maciDirPathName}`
+    const ethKeysDirPath = `${dirPath}/${ethDirPathName}`
+    const maciKeysDirPath = `${dirPath}/${maciDirPathName}`
 
-  if (quantity === 0) throw new Error(`Please, provide an amount greater than zero!`)
+    if (quantity === 0) throw new Error(`Please, provide an amount greater than zero!`)
 
-  // Create directories if not already generated.
-  if (!directoryExists(dirPath)) {
-    makeDir(dirPath)
-    makeDir(ethKeysDirPath)
-    makeDir(maciKeysDirPath)
-  } else {
-    cleanDir(ethKeysDirPath)
-    cleanDir(maciKeysDirPath)
+    // Create directories if not already generated.
+    if (!directoryExists(dirPath)) {
+      makeDir(dirPath)
+      makeDir(ethKeysDirPath)
+      makeDir(maciKeysDirPath)
+    } else {
+      cleanDir(ethKeysDirPath)
+      cleanDir(maciKeysDirPath)
+    }
+
+    for (let i = 0; i < quantity; i += 1) {
+      // Generate an Ethereum keypair.
+      const ethKeyPair: KeyPair = generateEthereumKeyPair()
+
+      // Generate a MACI keypair.
+      const maciKeyPair: KeyPair = generateMaciKeyPair()
+
+      const ethKeyDirPath = `${ethKeysDirPath}/${ethBaseName}_${i}`
+      const maciKeyDirPath = `${maciKeysDirPath}/${maciBaseName}_${i}`
+
+      makeDir(ethKeyDirPath)
+      makeDir(maciKeyDirPath)
+
+      /* Store as plaintext */
+
+      // Eth.
+      toTextFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey)
+      toTextFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey)
+
+      // Maci.
+      toTextFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey)
+      toTextFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey)
+
+      /* Store as QR Code */
+
+      // Eth.
+      toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey, QRCodeFileType.PNG)
+      toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey, QRCodeFileType.SVG)
+      toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey, QRCodeFileType.UTF8)
+
+      toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey, QRCodeFileType.PNG)
+      toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey, QRCodeFileType.SVG)
+      toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey, QRCodeFileType.UTF8)
+
+      // Maci.
+      toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey, QRCodeFileType.PNG)
+      toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey, QRCodeFileType.SVG)
+      toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey, QRCodeFileType.UTF8)
+
+      toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey, QRCodeFileType.PNG)
+      toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey, QRCodeFileType.SVG)
+      toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey, QRCodeFileType.UTF8)
+
+      console.log(`Keypair #${i} - ${logSymbols.success} Ethereum / ${logSymbols.success} MACI`)
+    }
+
+    console.log(
+      `\nYou have successfully completed the key generation 🎊 You can find everything inside the \`${dirPath}/\` folder!`
+    )
+  } catch (err: any) {
+    console.log(`${logSymbols.error} Something went wrong: ${err}`)
   }
-
-  for (let i = 0; i < quantity; i += 1) {
-    // Generate an Ethereum keypair.
-    const ethKeyPair: KeyPair = generateEthereumKeyPair()
-
-    // Generate a MACI keypair.
-    const maciKeyPair: KeyPair = generateMaciKeyPair()
-
-    const ethKeyDirPath = `${ethKeysDirPath}/${ethBaseName}_${i}`
-    const maciKeyDirPath = `${maciKeysDirPath}/${maciBaseName}_${i}`
-
-    makeDir(ethKeyDirPath)
-    makeDir(maciKeyDirPath)
-
-    /* Store as plaintext */
-
-    // Eth.
-    toTextFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey)
-    toTextFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey)
-
-    // Maci.
-    toTextFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey)
-    toTextFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey)
-
-    /* Store as QR Code */
-
-    // Eth.
-    toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey, QRCodeFileType.PNG)
-    toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey, QRCodeFileType.SVG)
-    toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_sk`, ethKeyPair.privateKey, QRCodeFileType.UTF8)
-
-    toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey, QRCodeFileType.PNG)
-    toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey, QRCodeFileType.SVG)
-    toQRCodeFile(ethKeyDirPath, `${ethBaseName}_${i}_pk`, ethKeyPair.publicKey, QRCodeFileType.UTF8)
-
-    // Maci.
-    toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey, QRCodeFileType.PNG)
-    toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey, QRCodeFileType.SVG)
-    toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_sk`, maciKeyPair.privateKey, QRCodeFileType.UTF8)
-
-    toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey, QRCodeFileType.PNG)
-    toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey, QRCodeFileType.SVG)
-    toQRCodeFile(maciKeyDirPath, `${maciBaseName}_${i}_pk`, maciKeyPair.publicKey, QRCodeFileType.UTF8)
-
-    console.log(`Keypair #${i} - ${logSymbols.success} Ethereum / ${logSymbols.success} MACI`)
-  }
-
-  console.log(
-    `\nYou have successfully completed the key generation 🎊 You can find everything inside the \`${dirPath}/\` folder!`
-  )
 }
 
 export default genkeys
