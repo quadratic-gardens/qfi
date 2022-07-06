f72f4506c124d9cb3e023be6f81176c1ca91c3ef
daodesigner <designerdao.eth@gmail.com>
2022-07-06 08:26:34 +0200

    nit: copy change on settings screen

--- packages/app/src/components/prague/Settings.tsx
+++ packages/app/src/components/prague/Settings.tsx
@@ -27,23 +27,24 @@ export const Settings = () => {
       <VStack mt={20} spacing={10} h="full" alignItems="flex-start">
         <VStack spacing={6} alignItems="flex-start">
           <Heading size="2xl">How it works?</Heading>
-          <Text >
-            Casting a Ballot requires you to have a wallet on Polygon Chain, MATIC to pay for gas, and a valid ballot key. Voting is anonymous, but ballot registration happens in person as a sybil check. At the event, visit the Quadratic Funding booth to get your ballot keys.
-          </Text>
+         
         </VStack>
         <VStack spacing={6} alignItems="flex-start">
           <Heading size="md">Polygon (MATIC) Wallet</Heading>
           <Text >
-            You will receive keys for an MATIC address that is whitelisted for this voting round, pre-loaded with enough gas to cover the transaction fees for ballot submission.
+           
+          All wallets that hold a redeemed NFT ticket are able to cast a Ballot for which project should get the funding. Casting a Ballot requires you to have a Polygon/Etherum wallet with some MATIC to pay for the transaction gas (a small amount of MATIC will be airdropped to all NFT ticket holders) and a valid ballot key. Voting is pseudoanonymous, but to submit your Ballot you will be sent your MACI key to your email address by the ETHBarcelona team. 
+          
           </Text>
           <MagikButton />
         </VStack>
         <VStack spacing={2} alignItems="flex-start">
           <Heading size="md">Ballot (MACI) passphrase</Heading>
           <Text >
-            MACI (Minimal Anti-Collusion Infrastructure) uses zero knowledge proofs to protect against censorship and collusion in blockchain voting.
-            Each voter gets a pseudo-random MACI key which is used to encrypt and validate your votes. This is the only way to vote in the round, and can be used to change your ballot at any time while the round is active, so keep it safe.
+          MACI (Minimal Anti-Collusion Infrastructure) uses zero knowledge proofs to protect against censorship and collusion in blockchain voting. (read more about MACI at this page). Each voter gets a pseudo-random MACI key which is used to encrypt and validate your votes. This is the only way to vote in the round, and can be used to change your ballot at any time while the round is active, so keep it safe (”not you MACI, not your vote”)
           </Text>
+
+         
         </VStack>
 
         <VStack spacing={1} alignItems="flex-start">
@@ -170,13 +171,13 @@ export const Settings = () => {
         <VStack spacing={2} alignItems="flex-start">
           <Heading size="md">Confused or need help?</Heading>
           <Text fontSize="md" >
-            You can send us an email at privacy.scaling.explorations@gmail.com or join the ETHBarcelona Telegram group
+          You can send us an email at qf@ethbarcelona.com or join the ETHBarcelona Telegram group use the #QF hashtag in your post https://t.me/ethbarcelona
             <Link href='https://t.me/ethbarcelona' isExternal>
               <Icon as={HiExternalLink} boxSize={4} color="gray.500" />
             </Link>
           </Text>
-          <Text fontSize="md" >
-            Visit us at the Quadratic Funding booth in the Looks Rare sponsor area at ETHBarcelona.
+          <Text >
+          During the event you can ask questions to the team behind this funding process and make have a workaround if you are not able to use the MACI key sent to your email. The team will be on stage to briefly present this process during the event, if in doubt ask any of the ETHBarcelona volunteers to point you to them if you have questions.
           </Text>
         </VStack>
       </VStack>
