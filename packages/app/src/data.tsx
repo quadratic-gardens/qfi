a9288fec34af67dc498306d29ab1d21cb795a10f
Yi-Cyuan Chen <emn178@gmail.com>
2022-09-20 16:52:24 +0800

    update data

--- packages/app/src/data.tsx
+++ packages/app/src/data.tsx
@@ -3,44 +3,44 @@ let projects = [
   {
     name: "守護者聯盟2.0",
     tagline:
-      "Through coaching and mentoring activities, we support, guide and motivate vulnerable young people to continue studying for a better future.",
+      "我們是「守護者聯盟2.0」，分別是由公、私部門及NGO共同組成的團隊， 我們的理念是運用科技，結合民間彼此的力量來配合政府，一同守護社會安全，用實際的行動用心守護我們珍視的每一個人，所以我們提出了「安居永續平安福」計畫，希望能達成全民安居、服務永續、平安是福的目標",
     description:
       "我們是「守護者聯盟2.0」，分別是由公、私部門及NGO共同組成的團隊， 我們的理念是運用科技，結合民間彼此的力量來配合政府，一同守護社會安全，用實際的行動用心守護我們珍視的每一個人，所以我們提出了「安居永續平安福」計畫，希望能達成全民安居、服務永續、平安是福的目標",
       problemSpace:
       "我們是「守護者聯盟2.0」，分別是由公、私部門及NGO共同組成的團隊， 我們的理念是運用科技，結合民間彼此的力量來配合政府，一同守護社會安全，用實際的行動用心守護我們珍視的每一個人，所以我們提出了「安居永續平安福」計畫，希望能達成全民安居、服務永續、平安是福的目標",
     address: "0x162335bE6926c244b068f491A4bfbb11970E4ed6",
     url: "https://www.facebook.com/SFGuardians",
-    logo:   "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297865/ETHBarcelona/fundaci%C3%B3n_exit_thumbnail.jpg",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297868/ETHBarcelona/fundaci%C3%B3n_exit_banner.jpg",
+    logo:   "https://i.imgur.com/VeYMTDV.png",
+    banner: "https://i.imgur.com/VeYMTDV.png",
     recipientId: 1,
     id: "1",
   },
   {
     name: "文譯之間",
-    tagline: "Extracting knowledge from Impact DAO builders for new web3 entrants",
+    tagline: "文譯之間團隊，有不同領域的工程師：醫院、半導體業、資訊安全、系統整合等，協力合作夥伴有公部門與NGO。這兩年疫情影響，醫院配合政策常需要快速更新當下的公告，通常會透過圖卡的形式發佈，以利民眾轉貼分享，這也是目前公部門政策宣導最常用的一種形式，然而，圖卡不利於翻譯、被搜尋等，而我們提供一種更友善的服務。",
     description:
       "文譯之間團隊，有不同領域的工程師：醫院、半導體業、資訊安全、系統整合等，協力合作夥伴有公部門與NGO。這兩年疫情影響，醫院配合政策常需要快速更新當下的公告，通常會透過圖卡的形式發佈，以利民眾轉貼分享，這也是目前公部門政策宣導最常用的一種形式，然而，圖卡不利於翻譯、被搜尋等，而我們提供一種更友善的服務。",
       problemSpace:
       "文譯之間團隊，有不同領域的工程師：醫院、半導體業、資訊安全、系統整合等，協力合作夥伴有公部門與NGO。這兩年疫情影響，醫院配合政策常需要快速更新當下的公告，通常會透過圖卡的形式發佈，以利民眾轉貼分享，這也是目前公部門政策宣導最常用的一種形式，然而，圖卡不利於翻譯、被搜尋等，而我們提供一種更友善的服務。",
     address: "0xb6e780438882f2daa11dA0972807f4D12166af8b",
     url: "https://inkrosetta.tw/",
-    logo: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297874/ETHBarcelona/impact_dao_thumbnail.jpg",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297887/ETHBarcelona/impact_dao_banner.jpg",
+    logo: "https://i.imgur.com/lrt2lEi.png",
+    banner: "https://i.imgur.com/lrt2lEi.png",
     recipientId: 2,
     id: "2",
   },
   {
     name: "CEASEFIRE",
     tagline:
-      "The DoinGud impact protocol enables anyone to participate in the decentralized funding, governance, and quality control of impact communities",
+      "解方主要效益：使林火發生後在最短時間偵測到，火勢初生即有效抑制和撲滅，使森林不受破壞，繼續發揮其生態及固碳功能。建議林務局成立林火消防救護基金會，作為土地活化平台，釋出國家未充分使用林地，供企業認養。基金會成員，由產官學民共同參與，Ceasefire團隊自願為創始成員，為竹林地活化展開新的合作模式。",
     description:
       "解方主要效益：使林火發生後在最短時間偵測到，火勢初生即有效抑制和撲滅，使森林不受破壞，繼續發揮其生態及固碳功能。建議林務局成立林火消防救護基金會，作為土地活化平台，釋出國家未充分使用林地，供企業認養。基金會成員，由產官學民共同參與，Ceasefire團隊自願為創始成員，為竹林地活化展開新的合作模式。",
       problemSpace:
       "解方主要效益：使林火發生後在最短時間偵測到，火勢初生即有效抑制和撲滅，使森林不受破壞，繼續發揮其生態及固碳功能。建議林務局成立林火消防救護基金會，作為土地活化平台，釋出國家未充分使用林地，供企業認養。基金會成員，由產官學民共同參與，Ceasefire團隊自願為創始成員，為竹林地活化展開新的合作模式。",
     address: "0xbd42cb82Bd03a194860882DE1Ff588f2B63BE45C",
     url: "https://www.linkedin.com/in/lin-harrison-378550b1/",
-    logo: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297866/ETHBarcelona/doingud_thumbnail.jpg",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297865/ETHBarcelona/doingud_banner.jpg",
+    logo: "https://i.imgur.com/5YvClTk.png",
+    banner: "https://i.imgur.com/5YvClTk.png",
     recipientId: 3,
     id: "3",
     
@@ -48,30 +48,30 @@ let projects = [
   {
     name: "透明足跡",
     tagline:
-      "TECHO  is a youth-led non-profit organization that works in Latin American informal settlements to create a just society without poverty.",
+      "綠色公民行動聯盟透明足跡計畫致力於透過資訊公開與應用，推動政府修正環境政策、促使企業邁向永續轉型。目前已有的資料應用有：推動政府公開環境裁罰資料，開發監督企業污染改善的透明足跡網站。並透過進一步資料處理，找出政府未掌握的高污染風險農地工廠。另外，透明足跡也整合企業永續資料，開發ESG 檢測儀，檢視企業永續表現。並在ESG檢測儀基礎上，進一步開發淨零路徑模擬器，檢視企業減碳目標與成效，離積極減量路徑還有多少距離。透明足跡期待透過資料應用降低公民參與的門檻，讓每個人都能參與，一起為更好的生活環境努力。",
     description:
       "綠色公民行動聯盟透明足跡計畫致力於透過資訊公開與應用，推動政府修正環境政策、促使企業邁向永續轉型。目前已有的資料應用有：推動政府公開環境裁罰資料，開發監督企業污染改善的透明足跡網站。並透過進一步資料處理，找出政府未掌握的高污染風險農地工廠。另外，透明足跡也整合企業永續資料，開發ESG 檢測儀，檢視企業永續表現。並在ESG檢測儀基礎上，進一步開發淨零路徑模擬器，檢視企業減碳目標與成效，離積極減量路徑還有多少距離。透明足跡期待透過資料應用降低公民參與的門檻，讓每個人都能參與，一起為更好的生活環境努力。",
       problemSpace:
       "綠色公民行動聯盟透明足跡計畫致力於透過資訊公開與應用，推動政府修正環境政策、促使企業邁向永續轉型。目前已有的資料應用有：推動政府公開環境裁罰資料，開發監督企業污染改善的透明足跡網站。並透過進一步資料處理，找出政府未掌握的高污染風險農地工廠。另外，透明足跡也整合企業永續資料，開發ESG 檢測儀，檢視企業永續表現。並在ESG檢測儀基礎上，進一步開發淨零路徑模擬器，檢視企業減碳目標與成效，離積極減量路徑還有多少距離。透明足跡期待透過資料應用降低公民參與的門檻，讓每個人都能參與，一起為更好的生活環境努力。",
     address: "0x8135206adF3221b0de578a6FB16Eb213Feb4E98E",
     url: "https://thaubing.gcaa.org.tw/",
-    logo: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297868/ETHBarcelona/techno_thumbnail.jpg",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297866/ETHBarcelona/techno_banner.jpg",
+    logo: "https://i.imgur.com/jJRlydo.png",
+    banner: "https://i.imgur.com/jJRlydo.png",
     recipientId: 4,
     id: "4",
   },
   {
     name: "Turing Certs",
     tagline:
-      "Readl is the platform dedicated to storytelling in Web3 that empowers authors, artists and publishers to create engaging stories and NFT Art.",
+      "圖靈證書（Turing Certs）正在世界解決造假問題，透過區塊鏈為所有人建立一個數位身分與歷程檔案。目前已經有 9 個國家與地區的 120 間的政府、學校、機構開始使用我們的數位憑證工具，發行上萬張的數位證書，解決畢業證書、專業能力證明、不動產合約書、農產品歷程、綠電憑證、碳權憑證。讓我們一起建立一個無紙化、安全隱私、100% 零碳數位化的未來。",
     description:
       "圖靈證書（Turing Certs）正在世界解決造假問題，透過區塊鏈為所有人建立一個數位身分與歷程檔案。目前已經有 9 個國家與地區的 120 間的政府、學校、機構開始使用我們的數位憑證工具，發行上萬張的數位證書，解決畢業證書、專業能力證明、不動產合約書、農產品歷程、綠電憑證、碳權憑證。讓我們一起建立一個無紙化、安全隱私、100% 零碳數位化的未來。",
       problemSpace:
       "圖靈證書（Turing Certs）正在世界解決造假問題，透過區塊鏈為所有人建立一個數位身分與歷程檔案。目前已經有 9 個國家與地區的 120 間的政府、學校、機構開始使用我們的數位憑證工具，發行上萬張的數位證書，解決畢業證書、專業能力證明、不動產合約書、農產品歷程、綠電憑證、碳權憑證。讓我們一起建立一個無紙化、安全隱私、100% 零碳數位化的未來。",
     address: "0xad0c8fb00464b9364eff43dce4a377542fc999e2",
     url: "https://certs.turingchain.tech",
-    logo: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657303859/ETHBarcelona/readl_thumbnail.jpg",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297864/ETHBarcelona/readl_banner.jpg",
+    logo: "https://i.imgur.com/GYMP6Ti.png",
+    banner: "https://i.imgur.com/GYMP6Ti.png",
     recipientId: 5,
     id: "5",
   },
@@ -79,15 +79,15 @@ let projects = [
   {
     name: "FinTech大聯盟",
     tagline:
-      "Lamina POP is a social enterprise that aims to increase access to low-cost, dignified housing worldwide through the use of our patented technology.",
+      "FinTech大聯盟旨在是透過履行社會責任，了解人民真實需求，作為銜接人民與政府之監理協作及監理溝通之橋樑，並透過政府授權或合作來提前反映爭點，降低未來新創與沙盒試驗之難度，同時因為教育之落實，理論與實作之經驗取得，金融科技教育得以向下普遍扎根。",
     description:
       "FinTech大聯盟旨在是透過履行社會責任，了解人民真實需求，作為銜接人民與政府之監理協作及監理溝通之橋樑，並透過政府授權或合作來提前反映爭點，降低未來新創與沙盒試驗之難度，同時因為教育之落實，理論與實作之經驗取得，金融科技教育得以向下普遍扎根。",
       problemSpace:
       "FinTech大聯盟旨在是透過履行社會責任，了解人民真實需求，作為銜接人民與政府之監理協作及監理溝通之橋樑，並透過政府授權或合作來提前反映爭點，降低未來新創與沙盒試驗之難度，同時因為教育之落實，理論與實作之經驗取得，金融科技教育得以向下普遍扎根。",
     address: "0x50b6a381993834C623b2Bded6825824C936E48bB",
     url: "https://presidential-hackathon.mic.org.tw/2020/ProposedContent.aspx",
-    logo: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297865/ETHBarcelona/lamina_pop_thumbnail.png",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297876/ETHBarcelona/lamina_pop_banner.png",
+    logo: "https://i.imgur.com/IX08sm4.png",
+    banner: "https://i.imgur.com/IX08sm4.png",
     recipientId: 6,
     id: "6",
   },
@@ -95,15 +95,15 @@ let projects = [
   {
     name: "海闊天空隊",
     tagline:
-      "Cohere is a community-owned global network of regenerative communities and innovation hubs powered by web3",
+      "我們是一群熱愛海洋的人們，來自於學校、企業、NGOs、相關政府單位，我們共同看見台灣的海洋面臨著危險，平常一起參與淨灘的活動中，發現海洋廢棄物可以重複再利用，經過企業夥伴們的嘗試，提出藍色經濟循環，希望將更多海廢清出大海，減少對海洋持續的破壞，並將每個環節都上鏈紀錄，為我們的海洋環境作出一些努力及貢獻。",
     description:
       "我們是一群熱愛海洋的人們，來自於學校、企業、NGOs、相關政府單位，我們共同看見台灣的海洋面臨著危險，平常一起參與淨灘的活動中，發現海洋廢棄物可以重複再利用，經過企業夥伴們的嘗試，提出藍色經濟循環，希望將更多海廢清出大海，減少對海洋持續的破壞，並將每個環節都上鏈紀錄，為我們的海洋環境作出一些努力及貢獻。",
       problemSpace:
       "我們是一群熱愛海洋的人們，來自於學校、企業、NGOs、相關政府單位，我們共同看見台灣的海洋面臨著危險，平常一起參與淨灘的活動中，發現海洋廢棄物可以重複再利用，經過企業夥伴們的嘗試，提出藍色經濟循環，希望將更多海廢清出大海，減少對海洋持續的破壞，並將每個環節都上鏈紀錄，為我們的海洋環境作出一些努力及貢獻。",
     address: "0x06f849833D0869730Ee98eb6FB75B950b093287b",
     url: "https://presidential-hackathon.mic.org.tw/2020/ProposedContent.aspx",
-    logo: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297866/ETHBarcelona/cohere_thumbnail.png",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297866/ETHBarcelona/cohere_banner.jpg",
+    logo: "https://i.imgur.com/sTVocad.png",
+    banner: "https://i.imgur.com/sTVocad.png",
     recipientId: 7,
     id: "7",
   },
@@ -111,15 +111,15 @@ let projects = [
   {
     name: "街道醫生",
     tagline:
-      "A new type of media company governed by the community embedding the web3 values of collaboration, co-creation and ownership.",
+      "街道醫生團隊透過「全民參與街道改善決策方案平台」建置與民眾溝通的管道，藉由人行道現況、社經環境資料的大數據疊加分析以及民眾參與，找出最迫切需要改善之路段。我們結合社福團體-身心障礙者聯盟、中華視障聯盟的鞭策，政府單位-內政部營建署的資源及補助機制、新北市政府的案件配合執行，產業界-鼎漢工程顧問公司、台科大資工系的技術指導，更結合民眾倡議並通過中央政府經費協助，形成街道永續安全的共識，逐一排除街道步行阻礙，打通街道經脈，讓民眾都能夠通行無礙。",
     description:
       "街道醫生團隊透過「全民參與街道改善決策方案平台」建置與民眾溝通的管道，藉由人行道現況、社經環境資料的大數據疊加分析以及民眾參與，找出最迫切需要改善之路段。我們結合社福團體-身心障礙者聯盟、中華視障聯盟的鞭策，政府單位-內政部營建署的資源及補助機制、新北市政府的案件配合執行，產業界-鼎漢工程顧問公司、台科大資工系的技術指導，更結合民眾倡議並通過中央政府經費協助，形成街道永續安全的共識，逐一排除街道步行阻礙，打通街道經脈，讓民眾都能夠通行無礙。",
       problemSpace:
       "街道醫生團隊透過「全民參與街道改善決策方案平台」建置與民眾溝通的管道，藉由人行道現況、社經環境資料的大數據疊加分析以及民眾參與，找出最迫切需要改善之路段。我們結合社福團體-身心障礙者聯盟、中華視障聯盟的鞭策，政府單位-內政部營建署的資源及補助機制、新北市政府的案件配合執行，產業界-鼎漢工程顧問公司、台科大資工系的技術指導，更結合民眾倡議並通過中央政府經費協助，形成街道永續安全的共識，逐一排除街道步行阻礙，打通街道經脈，讓民眾都能夠通行無礙。",
     address: "0xED835CB39712805913ffE3E6542Ac5e90FF981c5",
     url: "https://presidential-hackathon.mic.org.tw/2022/ProposedContent.aspx",
-    logo: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297865/ETHBarcelona/metso_thumbnail.jpg",
-    banner: "https://res.cloudinary.com/pse-qf-maci/image/upload/v1657297876/ETHBarcelona/lamina_pop_banner.png",
+    logo: "https://i.imgur.com/5mG9V0b.png",
+    banner: "https://i.imgur.com/5mG9V0b.png",
     recipientId: 8,
     id: "8",
   }
